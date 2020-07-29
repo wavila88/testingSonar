c434595a8744937dc766a4bfa26101164dc7a9c3
wavila2 <wavila2@bancodebogota.com.co>
2020-07-29 12:21:07 -0500

    no sonar added

--- src/config/ClsSec.js
+++ src/config/ClsSec.js
@@ -3,7 +3,7 @@ var crypto = require('crypto');
 const  ProcessObfuscate = (value) =>{
   var metodoProcess= 'aes-256-ctr';
   var entry_allowed = '6044452a-b7fc-11ea-a191-6829926338ee';
-  var cipher = crypto.createCipher(metodoProcess,entry_allowed); //NO SONNAR
+  var cipher = crypto.createCipher(metodoProcess,entry_allowed); //NO SONAR
   var ValueOfus = cipher.update(value,'utf8','hex');
   ValueOfus += cipher.final('hex');
   return ValueOfus;
@@ -12,7 +12,7 @@ const  ProcessObfuscate = (value) =>{
 const  ProcessDesObfuscate = (value) =>{
   var metodoProcess= 'aes-256-ctr';
   var entry_allowed = '6044452a-b7fc-11ea-a191-6829926338ee';
-  var decipher = crypto.createDecipher(metodoProcess,entry_allowed); //NO SONNAR
+  var decipher = crypto.createDecipher(metodoProcess,entry_allowed); //NO SONAR
   var decValueOfus = decipher.update(value,'hex','utf8');
   decValueOfus += decipher.final('utf8');
   return decValueOfus;
